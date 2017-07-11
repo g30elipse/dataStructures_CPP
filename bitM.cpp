f7004faaf03a5dde4989282ad72fa99bec3907f1
Mukut Brahma <mukutb.mac@gmail.com>
2017-07-11 16:55:25 +0530

    added more functions

--- bitM.cpp
+++ bitM.cpp
@@ -1,11 +1,25 @@
 
 /** Bit manipulation
   * isPowerOfTwo:		checks if the number 'n' is a power of 2
+  * countOnes:			returns: number of ones in the binary representation of 'n'
+  * bitCheck:			check if the ith bit is set in the binary form of  'n'
+  * checkAllBits:		checks if all the bits are set in the binary form of 'n'
 ***/
 
 #include <iostream>
 using namespace std;
 
+
+bool checkAllBits(int n)
+{
+	while(n)
+	{
+		if(!(n & 1))
+			return false;
+		n = n>>1;
+	}
+	return true;
+}
 
 
 bool isPowerOfTwo(int x)
@@ -13,6 +27,10 @@ bool isPowerOfTwo(int x)
 	return (x && !(x & (x-1)));
 }
 
+bool bitCheck(int n, int i)
+{
+	return (n & (1<<(i-1)));
+}
 
 int countOnes(int n)
 {
@@ -25,6 +43,20 @@ int countOnes(int n)
 	return count;
 }
 
+/* Alternative method
+int countOnes(int n)
+{
+	int count=0;
+	while(n)
+	{
+		if(n & 1)
+			count++;
+		n = n>>1;
+	}
+	return count;
+}
+*/
+
 
 int main()
 {
@@ -32,6 +64,11 @@ int main()
 	cin>>n;
 	
 	cout<<countOnes(n)<<endl;
+	
+	if(checkAllBits(n))
+		cout<<"All bits are set";
+	else
+		cout<<"All bits are not set";
 	
 	return 0;
 }
